175d47fa9ccf76f78ca4502010d226be7bbd3c66
reinisbarzdins <reinisbarzdins@gmail.com>
2026-01-29 21:48:02 +0200

    Initial commit V2

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import {defineConfig, globalIgnores} from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'timefold-quickstarts-stable']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -16,8 +17,64 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
+    },
+    plugins: {
+      react,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      eqeqeq: ['error', 'always'],
+      'no-console': 'off',
+      'no-debugger': 'warn',
+      'no-unused-expressions': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1,
+          ignoredNodes: ['JSXElement', 'JSXElement *'],
+        },
+      ],
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-indent-props': ['error', 2],
+      'no-tabs': 'error',
+      'max-len': [
+        'error',
+        {
+          code: 150,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreComments: false,
+        },
+      ]
     },
   },
 ])
